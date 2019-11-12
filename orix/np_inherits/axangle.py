Fix an off by one

orix/np_inherits/axangle.py
@@ -35,7 +35,7 @@ class AxAngle():
         acceptable angles and normalised vectors """
         if self.data.shape[1] != 4:
             raise ValueError("Your data is not in the correct shape")
-        if np.any(self.data[:,4] < 0) or np.any(self.data[:,4] > np.pi):
+        if np.any(self.data[:,3] < 0) or np.any(self.data[:,3] > np.pi):
             raise ValueError("Some of your angles lie outside of the range (0,pi)")
         if not np.allclose(np.linalg.norm(self.data[:,:3],axis=1),1):
             raise ValueError("You no longer have normalised direction vectors")