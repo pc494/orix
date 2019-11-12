68a12e1e237d6544da8e3a64bde8beea0336ca2b
pc494 <pc494@cam.ac.uk>
2019-11-12 18:39:21 +0000

    Facilitating the zero angle case

--- orix/np_inherits/axangle.py
+++ orix/np_inherits/axangle.py
@@ -40,7 +40,8 @@ class AxAngle():
             raise ValueError("Your data is not in the correct shape")
         if np.any(self.data[:,3] < 0) or np.any(self.data[:,3] > np.pi):
             raise ValueError("Some of your angles lie outside of the range (0,pi)")
-        if not np.allclose(np.linalg.norm(self.data[:,:3],axis=1),1):
+
+        if not np.allclose(np.linalg.norm(self.data[:,:3][self.data[3] > 0],axis=1),1):
             raise ValueError("You no longer have normalised direction vectors")
         return None
 
